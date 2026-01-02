9d8baccd2d89349809bf1650ec00a619af8e6bcd
alexpalmer <alex@platinumcs.ca>
2026-01-02 12:13:37 -0500

    Update sensor.py

--- components/gecko_spa/sensor.py
+++ components/gecko_spa/sensor.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import CONF_ID, STATE_CLASS_MEASUREMENT, DEVICE_CLASS_TEMPERATURE
+from esphome.const import CONF_ID, STATE_CLASS_MEASUREMENT
 from . import gecko_spa_ns, GeckoSpa
 
 DEPENDENCIES = ["gecko_spa"]
@@ -18,9 +18,8 @@ SENSOR_TYPES = {
 
 CONFIG_SCHEMA = sensor.sensor_schema(
     unit_of_measurement="d",
-    accuracy_decimals=1,
+    accuracy_decimals=0,
     state_class=STATE_CLASS_MEASUREMENT,
-    device_class=DEVICE_CLASS_TEMPERATURE,
 ).extend(
     {
         cv.GenerateID(CONF_GECKO_SPA_ID): cv.use_id(GeckoSpa),
