572d5857406bd2253fd042994446b56279621b43
alexpalmer <alex@platinumcs.ca>
2026-06-14 21:02:03 -0400

    Update gecko_spa.cpp

--- components/gecko_spa/gecko_spa.cpp
+++ components/gecko_spa/gecko_spa.cpp
@@ -870,9 +870,9 @@ void GeckoSpaClimate::setup() {
 
 climate::ClimateTraits GeckoSpaClimate::traits() {
   auto traits = climate::ClimateTraits();
-  traits.add_feature_flags(climate::CLIMATE_FEATURE_CURRENT_TEMPERATURE);
+  traits.add_feature_flags(climate::CLIMATE_SUPPORTS_CURRENT_TEMPERATURE);
   traits.set_supported_modes({climate::CLIMATE_MODE_HEAT, climate::CLIMATE_MODE_COOL});
-  traits.set_supports_action(true);
+  //traits.set_supports_action(true);
   traits.set_visual_min_temperature(26.0);
   traits.set_visual_max_temperature(40.0);
   traits.set_visual_temperature_step(0.5);
